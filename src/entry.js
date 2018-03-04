7a1e9b3545cc4fffad02620cc41b3530916803f3
Oscar Anton <oanton@gmail.com>
2018-03-04 20:07:14 +0100

    Single article page with some styles, json comments and form - form submit not working

--- src/entry.js
+++ src/entry.js
@@ -1,2 +1,17 @@
 import 'bootstrap';
 import css from './scss/style.scss';
+import { CommentsListController } from './js/CommentsListController';
+import { FormController } from './js/FormController';
+import { CommentsService } from './js/CommentsService';
+import { PubSub } from 'pubsub-js';
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // connect to the comments service on the selected data base
+    let commentsService = new CommentsService('http://localhost:3001/comments/');
+
+    // controls the display of the comments in the article
+    let commentsListController = new CommentsListController(".single-article__comments", commentsService, PubSub);
+    commentsListController.loadComments();
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,123 @@
-const path = require ('path');
-const webpack = require ('webpack');
+const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const isProduction = process.env.ENTORNO == "produccion";
+let scssLoaders = [];
+if (isProduction) {
+    scssLoaders = ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+                {
+                // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                loader: 'css-loader?url=false&sourceMap=true'
+            },
+                {
+                // Loader for webpack to process CSS with PostCSS
+                loader: 'postcss-loader?url=false&sourceMap=true',
+                options: {
+                    plugins: function () {
+                        return [
+                            require('autoprefixer')
+                        ];
+                    }
+                }
+            },
+            {
+                // Loads a SASS/SCSS file and compiles it to CSS
+                loader: 'sass-loader?sourceMap=true'
+            }
+        ]
+    });
+} else {
+    scssLoaders = [{
+        // Adds CSS to the DOM by injecting a `<style>` tag
+        loader: 'style-loader'
+    },
+    {
+        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+        loader: 'css-loader'
+    },
+    {
+        // Loader for webpack to process CSS with PostCSS
+        loader: 'postcss-loader',
+        options: {
+            plugins: function () {
+            return [
+                require('autoprefixer')
+            ];
+            }
+        }
+    },
+    {
+        // Loads a SASS/SCSS file and compiles it to CSS
+        loader: 'sass-loader'
+    }];
+}
 
 module.exports = {
 
-    // entry point
-    entry: path.join(__dirname, 'src', 'entry.js'),
+    // entry point: archivo que lee webpack para construir el grafo de dependencias
+    entry: ["babel-polyfill", path.join(__dirname, 'src', 'entry.js')],
 
-    // output
+    // output: carpeta en la que quiero que webpack me deje el código generado
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist')
     },
 
- // module loaders
- module: {
-    rules: [
-        {   // css modules (packaging and compiling)
-            test: /\.scss$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }, { // js retro-compatibility module
-            test: /\.js$/,
-            use: 'babel-loader',
-            exclude: path.join(__dirname, 'node_modules')
-        }, { // image filing and compression module in assets folder
-            test: /\.(jpe?g|png|gif|svg)$/,
-            use: [
-                'file-loader?name=[name].[ext]&useRelativePath=true',
-                'image-webpack-loader'
-            ]
-        }, { // files (not images) filing in assets folder
-            test: /assets.[^img]/,
-            use: 'file-loader?name=[name].[ext]&useRelativePath=true'
-        }
-    ]
-},
+    // module loaders
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: scssLoaders
+            }, {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: path.join(__dirname, 'node_modules')
+            }, {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: [
+                    'file-loader?name=[name].[ext]&useRelativePath=true',
+                    'image-webpack-loader'
+                ]
+            }, {
+                test: /assets.[^img]/,
+                use: 'file-loader?name=[name].[ext]&useRelativePath=true'
+            }
+        ]
+    },
 
-// plugins que estamos utilizando
-plugins: [
-    new HtmlWebpackPlugin({ // html packaging
-        template: path.join(__dirname, 'src', 'index.html'),
-        minify: { // html minify
-            collapseWhitespace: true
-        }
-    }),
-    new webpack.HotModuleReplacementPlugin(), // No funciona de momento
-    new webpack.NamedModulesPlugin() // No funciona de momento
-],
+    // plugins que estamos utilizando
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            minify: {
+                collapseWhitespace: true
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'article.html',
+            template: path.join(__dirname, 'src', 'article.html'),
+            minify: {
+                collapseWhitespace: true
+            }
+        }),
+        new ExtractTextPlugin('style.css')
+    ],
 
-// dev server configuration
-devServer: {
-    open: true, // abre el navegador por defecto
-    port: 3000, // puerto del servidor web
-    overlay: true, // muestra los errores en pantalla
-    hot: true, // No funciona de momento
-    contentBase: path.join(__dirname, 'src'), // No funciona de momento
-    watchContentBase: true // No funciona de momento
-}
+    // dev server configuration
+    devServer: {
+        open: true, // abre el navegador por defecto
+        port: 3000, // puerto del servidor web
+        overlay: true, // muestra los errores en pantalla
+        hot: true,
+        contentBase: [path.join(__dirname, 'src'), path.join(__dirname, 'src/includes')],
+        watchContentBase: true
+    }
 
 };
